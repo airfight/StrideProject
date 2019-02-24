ef7836501e73f0c32d621120a5500f18b4b4d2b5
zgy朱光阳 <gy.zhu@ctrip.com>
2019-02-24 22:06:15 +0800

    init

---
+++ StrideProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StrideProject
+//
+//  Created by ForJade Giant on 2019/2/24.
+//  Copyright © 2019年 com.Giant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
